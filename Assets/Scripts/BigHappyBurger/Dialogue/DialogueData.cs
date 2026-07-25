a3f68026474509b6b940b5f6d462654ff5ff1760
0041 <34074119+zilinious@users.noreply.github.com>
2026-07-25 05:01:26 -0500

    Dialogue System
    It doesn't work. The UI Framework doesn't work, and so the UI doesn't. Presumably, if it did, the code would work, but there's no way of knowing.

--- Assets/Scripts/BigHappyBurger/Dialogue/DialogueData.cs
+++ Assets/Scripts/BigHappyBurger/Dialogue/DialogueData.cs
@@ -3,5 +3,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "DialogueData", menuName = "Scriptable Objects/DialogueData")]
 public class DialogueData : ScriptableObject
 {
-    public string dialogueText = "Hello there";
+    public string dialogueText    = "Default disgruntled driver";
+
+    public string introduction    = "Default welcome";
+
+    public string replyCompliment = "Default compliment";
+    public string replyUpdate     = "Default update";
+    public string replyWeather    = "Default weather";
+    public string replyPlans      = "Default plans";
 }
